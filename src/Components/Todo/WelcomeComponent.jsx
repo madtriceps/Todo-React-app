ea67e334314b40d5b9a27389865321021ebfab39
madtriceps <115035770+madtriceps@users.noreply.github.com>
2024-03-10 19:41:09 +0530

    Axios Backend Call

--- src/Components/Todo/WelcomeComponent.jsx
+++ src/Components/Todo/WelcomeComponent.jsx
@@ -1,11 +1,14 @@
 import {useParams, Link} from 'react-router-dom'
 import axios from 'axios'
+import { useState } from 'react'
 
 export default function WelcomeComponent(){
     const {username} = useParams()
+    
+    const [message, setMessage]= useState();
 
     function helloWorldAPI(){
-        console.log('Calling Hello World API ')
+        console.log('Calling Hello World API')
         axios.get('http://localhost:8080/hello-world')
             .then( (response)=> successfulResponse(response))
             .catch( (error) => errorResponse(error))
@@ -13,6 +16,7 @@ export default function WelcomeComponent(){
     }   
 
     function successfulResponse(response){
+        setMessage(response.data)
         console.log(response)
     }
     
@@ -27,6 +31,7 @@ export default function WelcomeComponent(){
                 <h4>Click here to see todos</h4>
                 <Link to='/todos'>My Todos</Link>
                 <button className='btn btn-success mx-5' onClick={helloWorldAPI}>Hello World API</button>
+                <div className='text-info'>{message}</div>
             </div>
         </div>
     )
